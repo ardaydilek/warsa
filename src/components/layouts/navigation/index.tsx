3345520822a406ce3057dc1d44b17097d309426c
Arda Kaan Aydilek <ardakaanaydilek@gmail.com>
2024-07-22 20:00:42 +0300

    test blog

--- src/components/layouts/navigation/index.tsx
+++ src/components/layouts/navigation/index.tsx
@@ -9,14 +9,12 @@ export default function Navigation({}) {
   return (
     <header className="bg-surface h-16 py-2 fixed w-full flex items-center z-[99] px-4 md:px-0">
       <nav className="md:container mx-auto w-full  flex justify-between items-center">
-        {/* Logo */}
         <>
           <Link
             href="/"
             className="w-44 xs:w-52 h-10 flex items-center text-primary shrink-0"
             data-hide-on-theme="dark"
           >
-            {/* <WarsaLight /> */}
             <Image
               src={"/warsa-light.svg"}
               alt="Warsa"
@@ -29,7 +27,6 @@ export default function Navigation({}) {
             className="w-44 xs:w-52 h-10 flex items-center text-primary shrink-0"
             data-hide-on-theme="light"
           >
-            {/* <WarsaDark /> */}
             <Image
               src={"/warsa-dark.svg"}
               alt="Warsa"
@@ -39,10 +36,8 @@ export default function Navigation({}) {
           </Link>
         </>
 
-        {/* Menu */}
         <NavigationLinkItems className="hidden lg:flex" />
 
-        {/* Buttons */}
         <NavigationButtons />
       </nav>
     </header>
