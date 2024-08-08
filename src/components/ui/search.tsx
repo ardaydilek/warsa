38a74cae88a9a64d4308776a07cd612bffe44c02
Arda Kaan Aydilek <ardakaanaydilek@gmail.com>
2024-08-08 18:02:57 +0300

    nav item

--- src/components/ui/search.tsx
+++ src/components/ui/search.tsx
@@ -17,11 +17,13 @@ import {
   CommandShortcut,
 } from "@/components/ui/command";
 import { LandCounties, LandData } from "@/lib/mock-data";
+import { useRouter } from "next/navigation";
 
 export type SearchProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Search = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
+    const router = useRouter();
     const [open, setOpen] = React.useState(false);
 
     React.useEffect(() => {
@@ -46,17 +48,17 @@ const Search = React.forwardRef<HTMLInputElement, InputProps>(
           {...props}
           type="search"
           ref={ref}
-          className="w-full placeholder:text-surface-on-variant placeholder:text-base placeholder:font-bold focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 bg-inherit font-bold text-surface-on-variant"
+          className="w-full bg-inherit font-bold text-surface-on-variant placeholder:text-base placeholder:font-bold placeholder:text-surface-on-variant focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           onClick={() => setOpen(true)}
         />
 
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100 mr-2">
+        <kbd className="bg-muted text-muted-foreground pointer-events-none mr-2 inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100">
           <span className="text-xs">⌘</span>K
         </kbd>
         <Button
           type="submit"
           size="icon"
-          className="min-w-[48px] w-12 h-12 rounded-full m-1"
+          className="m-1 size-12 min-w-[48px] rounded-full"
         >
           <SearchIcon className="text-surface-variant" />
         </Button>
@@ -70,21 +72,25 @@ const Search = React.forwardRef<HTMLInputElement, InputProps>(
             <CommandEmpty>Bir sonuç bulamadık.</CommandEmpty>
             <CommandGroup heading="Projeler">
               {LandData.map((item) => (
-                <CommandItem key={`land-item-${item.title}`}>
-                  <House className="mr-2 h-4 w-4" />
+                <CommandItem
+                  key={`land-item-${item.title}`}
+                  onSelect={() => router.push(`/lands/${item.slug}`)}
+                  className="cursor-pointer"
+                >
+                  <House className="mr-2 size-4" />
                   <span className="">{item.title}</span>
                 </CommandItem>
               ))}
             </CommandGroup>
             <CommandSeparator />
-            <CommandGroup heading="Şehirler">
+            {/* <CommandGroup heading="Şehirler">
               {LandCounties.map((item) => (
                 <CommandItem key={`land-item-${item}`}>
-                  <MapPin className="mr-2 h-4 w-4" />
+                  <MapPin className="mr-2 size-4" />
                   <span className="">{item}</span>
                 </CommandItem>
               ))}
-            </CommandGroup>
+            </CommandGroup> */}
           </CommandList>
         </CommandDialog>
       </div>
